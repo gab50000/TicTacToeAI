7db47d9f12d19b5dfeb6fa7530e785106379d1fa
Gabriel Kabbe <gabriel.kabbe@mail.de>
2021-08-29 18:19:30 +0200

    Setup test

--- tests/test_Board.cpp
+++ tests/test_Board.cpp
@@ -1,6 +1,11 @@
 #include "gtest/gtest.h"
 #include "tictactoe/Board.hpp"
 
-TEST(Board, ) {
-  EXPECT_TRUE(true);
+TEST(Board, GetEmptyFields) {
+  Board board;
+  auto empty_fields = board.get_empty_fields();
+  EXPECT_EQ(empty_fields.size(), 9);
+  board.set_field({0, 0}, Field::O);
+  empty_fields = board.get_empty_fields();
+  EXPECT_EQ(empty_fields.size(), 8);
 }
